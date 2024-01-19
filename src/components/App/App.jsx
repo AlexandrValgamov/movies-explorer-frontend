b0f3b73973d6bfb2a1260fdd6ed2abc42fbd0e43
AlexandrValgamov <valgamov2010@yandex.ru>
2024-01-19 12:16:33 +0500

    feat: add Header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import React, { useEffect } from "react";
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Header from "../Header/Header";
 
 function App() {
 
+  const [loggedIn, setLoggedIn] = React.useState(true);
+
   return (
-    <>
-      
-    </>
+    <div className="page">
+      <Header
+        loggedIn={loggedIn}
+      />
+      <Routes>
+        <Route
+          path="/"
+        // element={Main}
+        />
+      </Routes>
+
+    </div>
   )
 }
 
