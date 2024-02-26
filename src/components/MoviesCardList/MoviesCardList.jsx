a7de76d7601d8cf6dd731db53800ccd92afcc23b
AlexandrValgamov <valgamov2010@yandex.ru>
2024-02-26 22:08:29 +0500

    feat: make MoviesCardList responsive

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,12 +1,32 @@
 import "./MoviesCardList.css";
 import PropTypes from 'prop-types';
 import MoviesCard from "../MoviesCard/MoviesCard";
+import { useEffect, useState } from "react";
 
 export default function MoviesCardList({ cards }) {
+  const [cardsCount, setCardsCount] = useState(16);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 760) {
+        setCardsCount(5)
+      } else if (window.innerWidth >= 900) {
+        setCardsCount(16)
+      } else {
+        setCardsCount(8)
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <section className="cards">
       <div className="cards__gallary" aria-label="Галерея">
-        {cards.slice(0, 16).map(
+        {cards.slice(0, cardsCount).map(
           card => <MoviesCard
             key={card.id}
             card={card}
