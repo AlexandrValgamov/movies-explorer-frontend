892cff860ce60d2a92e5aaa4ed2e24ec2e143cce
AlexandrValgamov <valgamov2010@yandex.ru>
2024-04-07 21:15:56 +0500

    feat: add Form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,16 @@
+import './Form.css';
+import PropTypes from 'prop-types';
+
+export default function Form({ onSubmit, name, children }) {
+  return (
+    <form className="form" name={name} onSubmit={onSubmit}>
+      {children}
+    </form>
+  );
+}
+
+Form.propTypes = {
+  onSubmit: PropTypes.func,
+  name: PropTypes.string,
+  children: PropTypes.node,
+};
